b729b69b1a9a853dd80eb8d281ff00a48ccdc142
arumicube <aryum2003@gmail.com>
2025-01-01 18:12:27 +0900

    firstTestSpring

---
+++ src/test/java/TestSpring/SpringBootTestApplicationTests.java
@@ -0,0 +1,15 @@
+package TestSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // 기본 테스트 메서드
+    }
+}
+
+
