6ad2914aac4de45e78cfd71ba80cbb24b004df9a
arumicube <aryum2003@gmail.com>
2025-01-02 19:50:22 +0900

    second2

---
+++ hello-spring/src/main/java/TestSpring/controller/DemoController.java
@@ -0,0 +1,13 @@
+package TestSpring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "Hi, my name is aryum AN.";
+    }
+}
